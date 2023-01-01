9e7992ccc7c109c4476107db8c94d29e29723ac3
Enegg <42005170+Enegg@users.noreply.github.com>
2023-01-01 18:47:50 +0100

    make the path relative

--- script.ps1
+++ script.ps1
@@ -1,3 +1,2 @@
-﻿cd C:\Real_Shit_Estate\Item-packs
-python C:\Real_Shit_Estate\Item-packs\count_missing_data.py
-python C:\Real_Shit_Estate\Item-packs\new.py
+﻿python count_missing_data.py
+python new.py
